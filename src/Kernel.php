abfeff35fed1723a6802b2653eab21a41e2571c2
thibaultg <35468476+Crovitche-1623@users.noreply.github.com>
2021-01-06 17:45:31 +0100

    .idea exclusion, fix indentation, static analysis and add correct infos in composer.json

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,37 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use function dirname;
 
-class Kernel extends BaseKernel
+final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configureContainer(ContainerConfigurator $container): void
     {
+        /**
+         * @var  string $environment
+         */
+        $environment = $this->environment;
         $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../config/{packages}/' . $environment . '/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/services.yaml')) {
             $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
+            $container->import('../config/{services}_' . $environment . '.yaml');
+        } elseif (is_file($path = dirname(__DIR__) . '/config/services.php')) {
+            /** @noinspection PhpIncludeInspection */
             (require $path)($container->withPath($path), $this);
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        /**
+         * @var  string $environment
+         */
+        $environment = $this->environment;
+        $routes->import('../config/{routes}/' . $environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
+        } elseif (is_file($path = dirname(__DIR__) . '/config/routes.php')) {
+            /** @noinspection PhpIncludeInspection */
             (require $path)($routes->withPath($path), $this);
         }
     }
